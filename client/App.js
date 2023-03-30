16b8aa512a98fb70ce3cea833c4731bc05be6219
Dominic <dominiccao23@gmail.com>
2023-03-30 18:49:37 -0400

    updated schemas and debugged files

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './components/signIn'
-import SignUp from './components/signUp';
+import SignIn from './src/components/signIn'
+import SignUp from './src/components/signUp';
 function App() {
   return (
     <h1 className="text-3xl font-bold text-blue-500">
