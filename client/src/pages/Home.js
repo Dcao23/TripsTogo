72186033b928197e442454106bd2cd630b29f2c2
jag cheema <jagmitsingh2003@gmail.com>
2023-04-03 15:42:47 -0400

    adding more info boxes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,6 +22,21 @@ export const Home = () => {
           img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/Banff-to-Jasper-Icefields-Parkway-Lake-Louise.jpg"
           description={`Lake Louise is a hamlet within Banff National Park in Alberta, Canada. Named after Princess Louise, Duchess of Argyll, it lies in Alberta's Rockies on the Bow River, 3 km (1.9 mi) northeast of the lake that shares its name. `}
         />
+        <InfoBox
+          title="Maldives"
+          img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/Adventure-couples-Maldives-Bungalow.jpg"
+          description={`The Maldives officially the Republic of Maldives Dhivehi Raajjeyge Jumhooriyyaa, Dhivehi, is an archipelagic state in South Asia, situated in the Indian Ocean.`}
+        />
+        <InfoBox
+          title="Antarctica"
+          img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/Facts-About-New-Zealand-Antarctica-Neighbour.jpg"
+          description={`Antarctica is Earth's southernmost and least-populated continent. Situated almost entirely south of the Antarctic Circle and surrounded by the Southern Ocean (also known as the Antarctic Ocean), it contains the geographic South Pole.`}
+        />
+        <InfoBox
+          title="Serengeti Plains, Tanzania"
+          img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/photography-tours-2018-tanzania-25.jpg"
+          description={`The Serengeti ecosystem is a geographical region in Africa, spanning the Mara and Arusha Regions of Tanzania.[1] The protected area within the region includes approximately 30,000 km2 (12,000 sq mi) of land, including the Serengeti National Park.`}
+        />
       </div>
     </div>
   );
