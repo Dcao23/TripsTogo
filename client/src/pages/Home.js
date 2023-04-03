781e1e17afffc59d0e37682b661369c1e6a1a490
jag cheema <jagmitsingh2003@gmail.com>
2023-04-03 15:18:17 -0400

    header implemented in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,10 @@
-
+import React from "react";
+import { Header } from "client/src/components/header.jsx";
+import "../assets/css/home.css";
+export const Home = () => {
+  return (
+    <div className="Home">
+      <Header />
+    </div>
+  );
+};
