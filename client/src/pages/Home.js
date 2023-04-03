7baeed269fc1f3721828bdd8f07c6db2c69f803e
jag cheema <jagmitsingh2003@gmail.com>
2023-04-03 15:37:29 -0400

    added 2 more info boxes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,6 +11,16 @@ export const Home = () => {
           title="Cappadocia, Turkey"
           img="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Cappadocia_balloon_trip%2C_Ortahisar_Castle_%2811893715185%29.jpg/516px-Cappadocia_balloon_trip%2C_Ortahisar_Castle_%2811893715185%29.jpg"
           description={`Cappadocia is a historical region in Central Anatolia, Turkey. It is largely in the provinces of Nevşehir, Kayseri, Aksaray, Kırşehir, Sivas and Niğde.`}
+        />{" "}
+        <InfoBox
+          title="Santorini"
+          img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/best-Things-to-do-in-Santorini-Greece-Bells-Oia.jpg"
+          description={`Santorini is an island in the southern Aegean Sea, about 200 km (120 mi) southeast from the Greek mainland. It is the largest island of a small circular archipelago, which bears the same name and is the remnant of a caldera.`}
+        />
+        <InfoBox
+          title="Lake Louise, Canada"
+          img="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/static/optimized/rev-939cb5a/images/Banff-to-Jasper-Icefields-Parkway-Lake-Louise.jpg"
+          description={`Lake Louise is a hamlet within Banff National Park in Alberta, Canada. Named after Princess Louise, Duchess of Argyll, it lies in Alberta's Rockies on the Bow River, 3 km (1.9 mi) northeast of the lake that shares its name. `}
         />
       </div>
     </div>
